30ef3d61de470295bdd2c89477e17124fde8005d
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-19 11:44:36 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
+import express, {Application, Request, Response, NextFunction, Router} from 'express';
 import cors from 'cors';
 import config from 'config';
+
+// Import routes
+import authRouter from './routes/auth';
+import urlRouter from './routes/url';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Use routes
+app.use('api/user', authRouter);
+app.use('/api/url', urlRouter);
 
 app.get('/', async (req: Request, res: Response) => {
     res.json({ message: "Server is working!" });
