cf921fbc8be5f277808936f225488c49c8fb4dfa
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-19 11:45:00 +0300

    Add auth.ts

---
+++ src/routes/auth.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response, Router} from 'express';
+const authRouter: Router = express.Router();
+
+authRouter.post('/signup', async (req: Request, res: Response) => {
+
+});
+
+authRouter.post('/login', async (req: Request, res: Response) => {
+
+});
+
+export default authRouter;
