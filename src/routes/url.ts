b8271b9643562cfef9342fedfd89ef3708edbf10
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-19 11:45:23 +0300

    Add url.ts

---
+++ src/routes/url.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from 'express';
+const urlRouter = express.Router();
+
+urlRouter.post('/', async (req: Request, res: Response) => {
+
+});
+
+urlRouter.get('/', async (req: Request, res: Response) => {
+
+});
+
+export default urlRouter;
