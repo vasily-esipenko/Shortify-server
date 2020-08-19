9bee8b6eaf6b33e991105dd194a9930b47b09667
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-19 12:02:00 +0300

    Update url.ts

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -6,7 +6,7 @@ urlRouter.post('/', async (req: Request, res: Response) => {
 });
 
 urlRouter.get('/', async (req: Request, res: Response) => {
-
+    res.json({message: "Development in progress"});
 });
 
 export default urlRouter;
