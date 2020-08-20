b0f6d7129bec4427cec2a50b1e4f35f4f3669f94
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-20 12:31:57 +0300

    Update url.ts

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -1,12 +1,23 @@
 import express, {Request, Response} from 'express';
+import ShortUrl from '../models/shortUrl';
 const urlRouter = express.Router();
 
 urlRouter.post('/', async (req: Request, res: Response) => {
-
+    await ShortUrl.create({full: req.body.fullUrl});
+    res.json({message: "Url was added"});
 });
 
 urlRouter.get('/', async (req: Request, res: Response) => {
-    res.json({message: "Development in progress"});
+    const shortUrls = await ShortUrl.find();
+    res.json(shortUrls);
+});
+
+urlRouter.get('/:shortUrl', async (req: Request, res: Response) => {
+    const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl});
+    if (shortUrl == null) return res.sendStatus(404);
+
+    //shortUrl.clicks++;
+    shortUrl.save();
 });
 
 export default urlRouter;
